e1e36f6a59b82b00d98391306dce66d387261c8e
BryanGC96 <2014104@icum.edu.mx>
2024-06-02 19:27:38 -0600

    Added new '.gitignore' file, installed dependencies for apollo server, modified 'server.js' file inside server folder

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const {authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +17,47 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.use(routes); //? RESTfull routes
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+//? Creates a new instance os an Apollo server with the graphQL schema.
+const startApolloServer = async () => {
+  const server = new startApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+      const modifiedReq = authMiddleware({ req });
+      return { user: modifiedReq.user };
+    },
+  });
+
+  await server.start();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', server.getMiddleware({
+    context: ({ req }) => {
+      const modifiedReq = authMiddleware({ req });
+      return { user: modifiedReq.user };
+    },
+  }));
+
+  //? Serve static assets in production.
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+//? Calls the async func. to start the server.
+startApolloServer();
